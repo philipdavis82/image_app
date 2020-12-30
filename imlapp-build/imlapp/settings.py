19fc4e17f9fe122e6572f860da12075447204154
Philip Davis <philip.davis.me@gmail.com>
2020-12-30 16:44:26 -0600

    Added Seceret Key Generator

--- imlapp-build/imlapp/settings.py
+++ imlapp-build/imlapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  # TODO: Make a new secret key and store it locally
+SECRET_KEY =  get_random_secret_key()# TODO: Make a new secret key and store it locally
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
